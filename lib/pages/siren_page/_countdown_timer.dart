a8f52f95a6922586e66f42e533fb6c3c3414368d
Anton <anton@antons-webfabrik.eu>
2025-05-29 21:22:18 +0200

    feat(siren): add ability to start/stop siren countdown

--- lib/pages/siren_page/_countdown_timer.dart
+++ lib/pages/siren_page/_countdown_timer.dart
@@ -9,9 +9,16 @@ class _CountdownTimer extends StatefulWidget {
   State<_CountdownTimer> createState() => _CountdownTimerState();
 }
 
-class _CountdownTimerState extends State<_CountdownTimer> {
-  late int _remaining;
+class _CountdownTimerState extends State<_CountdownTimer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _borderColorController;
+  late Animation<Color?> _borderColorAnimation;
+
   late Timer _timer;
+
+  int? _remaining;
+
+  bool _didInitAnimation = false;
 
   @override
   void initState() {
@@ -19,11 +26,45 @@ class _CountdownTimerState extends State<_CountdownTimer> {
     _startTimer();
   }
 
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final WebfabrikThemeData theme = WebfabrikTheme.of(context);
+
+    if (!_didInitAnimation) {
+      _borderColorController = AnimationController(
+        duration: theme.durations.short,
+        vsync: this,
+      );
+
+      _borderColorController.addListener(() {
+        setState(() {});
+      });
+
+      _didInitAnimation = true;
+    } else {
+      _borderColorController.duration = theme.durations.xShort;
+    }
+
+    _borderColorAnimation =
+        ColorTween(begin: theme.colors.error, end: theme.colors.text).animate(
+          CurvedAnimation(
+            parent: _borderColorController,
+            curve: Curves.easeOut,
+            reverseCurve: Curves.easeIn,
+          ),
+        );
+  }
+
   void _startTimer() {
-    _remaining = widget.duration.inMilliseconds;
+    _remaining ??= widget.duration.inMilliseconds;
+
     _timer = Timer.periodic(Duration(milliseconds: 1), (timer) {
-      if (_remaining > 0) {
-        setState(() => _remaining--);
+      if (_remaining! > 0) {
+        setState(() {
+          _remaining = _remaining! - 1;
+        });
       } else {
         context.read<SirenCubit>().startSiren();
         timer.cancel();
@@ -40,31 +81,46 @@ class _CountdownTimerState extends State<_CountdownTimer> {
   @override
   Widget build(BuildContext context) {
     final WebfabrikThemeData theme = WebfabrikTheme.of(context);
-    return Stack(
-      children: [
-        SizedBox(
-          width: 240,
-          height: 240,
-          child: CircularProgressIndicator(
-            value: _remaining / widget.duration.inMilliseconds,
-            strokeWidth: 20,
-            strokeCap: StrokeCap.round,
-            color: theme.colors.error,
-            backgroundColor: theme.colors.fieldColor,
-          ),
-        ),
-        Positioned.fill(
-          child: Center(
-            child: Text(
-              (_remaining / 1000).toStringAsFixed(0),
-              style: theme.text.largeTitle.copyWith(
-                fontWeight: FontWeight.w400,
-                fontSize: 96,
+    return BlocConsumer<SirenCubit, SirenState>(
+      listener: (BuildContext context, SirenState sirenState) {
+        if (sirenState is SirenCountdownStopped) {
+          _borderColorController.forward();
+          _timer.cancel();
+        }
+
+        if (sirenState is SirenCountdown) {
+          _borderColorController.reverse();
+          _startTimer();
+        }
+      },
+      builder: (BuildContext context, SirenState sirenState) {
+        return Stack(
+          children: [
+            SizedBox(
+              width: 240,
+              height: 240,
+              child: CircularProgressIndicator(
+                value: _remaining! / widget.duration.inMilliseconds,
+                strokeWidth: 20,
+                strokeCap: StrokeCap.round,
+                color: _borderColorAnimation.value,
+                backgroundColor: theme.colors.fieldColor,
               ),
             ),
-          ),
-        ),
-      ],
+            Positioned.fill(
+              child: Center(
+                child: Text(
+                  (_remaining! / 1000).toStringAsFixed(0),
+                  style: theme.text.largeTitle.copyWith(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 96,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }

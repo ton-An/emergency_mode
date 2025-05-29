faf61c3dcf2342fd271182c8669a4989ad04db78
Anton <anton@antons-webfabrik.eu>
2025-05-29 16:36:42 +0200

    feat: improve styling of draggable selector and add on successful drag callback

--- lib/widgets/draggable_selector.dart
+++ lib/widgets/draggable_selector.dart
@@ -2,77 +2,202 @@ import 'package:flutter/cupertino.dart';
 import 'package:smooth_corner/smooth_corner.dart';
 import 'package:webfabrik_theme/webfabrik_theme.dart';
 
-class DraggableSelector extends StatelessWidget {
+class DraggableSelector extends StatefulWidget {
   const DraggableSelector({
     super.key,
     required this.label,
     required this.icon,
+    required this.onSuccessfulDrag,
     this.iconColor,
     this.handleColor,
   });
 
   final String label;
   final IconData icon;
+  final VoidCallback onSuccessfulDrag;
   final Color? iconColor;
   final Color? handleColor;
+
+  @override
+  State<DraggableSelector> createState() => _DraggableSelectorState();
+}
+
+class _DraggableSelectorState extends State<DraggableSelector>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _pullBackController;
+  late Animation<double> _pullBackAnimation;
+  late Duration _maxPullBackDuration;
+
+  bool _isVisible = true;
+
+  double _dragPosition = 0;
+
+  final GlobalKey _containerKey = GlobalKey();
+
+  static const double handleSize = 74;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final WebfabrikThemeData theme = WebfabrikTheme.of(context);
+
+    _maxPullBackDuration = theme.durations.medium;
+    _pullBackController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _pullBackAnimation = Tween<double>(
+      begin: -1,
+      end: 0,
+    ).animate(_pullBackController);
+
+    _pullBackController.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final WebfabrikThemeData theme = WebfabrikTheme.of(context);
 
-    return SmoothClipRRect(
-      borderRadius: BorderRadius.circular(72),
-      child: Container(
-        height: 74,
-        color: theme.colors.translucentBackgroundContrast,
+    return AnimatedOpacity(
+      duration: theme.durations.short,
+      curve: Curves.ease,
+      opacity: _isVisible ? 1 : 0,
+      child: Align(
+        alignment: Alignment.centerRight,
         child: Stack(
           children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.all(4.3),
-                child: SmoothClipRRect(
-                  borderRadius: BorderRadius.circular(72),
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: Container(
-                      height: double.infinity,
-                      color: handleColor ?? theme.colors.background,
-                      child: Icon(
-                        icon,
-                        size: 38,
-                        color: iconColor ?? theme.colors.error,
+            Container(
+              key: _containerKey,
+              height: handleSize,
+              width: double.infinity,
+              margin: EdgeInsets.only(
+                left: _pullBackController.isAnimating
+                    ? _pullBackAnimation.value
+                    : _dragPosition,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(handleSize),
+                color: theme.colors.translucentBackgroundContrast,
+              ),
+              child: GestureDetector(
+                onHorizontalDragStart: (details) {
+                  setState(() {
+                    _dragPosition = 0;
+                  });
+                },
+                onHorizontalDragUpdate: (details) {
+                  final double delta = details.delta.dx;
+                  final double newPosition = _dragPosition + delta;
+                  final double containerWidth =
+                      _containerKey.currentContext?.size?.width ?? 0;
+                  final double draggableWidth = containerWidth - handleSize;
+                  final double clampedPosition = newPosition.clamp(
+                    0,
+                    draggableWidth,
+                  );
+
+                  setState(() {
+                    _dragPosition = clampedPosition;
+                  });
+                },
+                onHorizontalDragEnd: (details) async {
+                  final double containerWidth =
+                      _containerKey.currentContext?.size?.width ?? 0;
+                  final double draggableWidth = containerWidth - handleSize;
+
+                  if (_dragPosition >= draggableWidth - 60) {
+                    setState(() {
+                      _isVisible = false;
+                    });
+                    await Future.delayed(Duration(milliseconds: 350));
+                    widget.onSuccessfulDrag();
+                  } else {
+                    _pullBackAnimation = Tween<double>(
+                      begin: _dragPosition,
+                      end: 0,
+                    ).animate(_pullBackController);
+                    final double draggedDistancePercentage =
+                        _dragPosition / draggableWidth;
+                    final Duration animationDuration =
+                        _maxPullBackDuration * draggedDistancePercentage;
+                    _pullBackController.value = 0;
+                    _pullBackController.duration = animationDuration;
+                    _pullBackController.forward();
+                    _dragPosition = 0;
+                  }
+                },
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.all(4.3),
+                    child: SmoothClipRRect(
+                      borderRadius: BorderRadius.circular(72),
+                      child: AspectRatio(
+                        aspectRatio: 1,
+                        child: Container(
+                          height: double.infinity,
+                          color: widget.handleColor ?? theme.colors.background,
+                          child: Icon(
+                            widget.icon,
+                            size: 38,
+                            color: widget.iconColor ?? theme.colors.error,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-            Row(
-              children: [
-                Expanded(flex: 9, child: SizedBox.expand()),
-                Expanded(
-                  flex: 40,
-                  child: Center(
-                    child: Text(
-                      label,
-                      style: TextStyle(
-                        fontFamily: '.SF Pro Display',
-                        fontSize: 21,
-                        fontWeight: FontWeight.w500,
-                        height: 25 / 21,
-                        letterSpacing: -1.3,
-                        wordSpacing: 2,
-                        color: theme.colors.background,
+            Positioned.fill(
+              child: Row(
+                children: [
+                  Expanded(flex: 9, child: SizedBox.expand()),
+                  Expanded(
+                    flex: 40,
+                    child: Center(
+                      child: AnimatedOpacity(
+                        duration: theme.durations.tiny,
+                        opacity: _calculateHandlePosition() < 35 ? 1 : 0,
+                        child: AnimatedOpacity(
+                          duration: theme.durations.short,
+                          opacity: _calculateHandlePosition() < 20 ? 1 : 0,
+                          child: Text(
+                            widget.label,
+                            style: TextStyle(
+                              fontFamily: '.SF Pro Display',
+                              fontSize: 21,
+                              fontWeight: FontWeight.w500,
+                              height: 25 / 21,
+                              letterSpacing: -1.3,
+                              wordSpacing: 2,
+                              color: theme.colors.background,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  double _calculateHandlePosition() {
+    late double handlePosition;
+    if (_pullBackController.isAnimating) {
+      handlePosition = _pullBackAnimation.value;
+    } else {
+      handlePosition = _dragPosition;
+    }
+
+    return handlePosition;
   }
 }

588184c5bd23ee6bcd6e6e4fd6241a1eb0be954f
Anton <anton@antons-webfabrik.eu>
2025-05-30 14:43:37 +0200

    feat: add x large button

---
+++ lib/widgets/x_large_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/widgets.dart';
+import 'package:smooth_corner/smooth_corner.dart';
+import 'package:webfabrik_theme/webfabrik_theme.dart';
+
+class XLargeButton extends StatelessWidget {
+  const XLargeButton({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.onPressed,
+  });
+
+  final IconData icon;
+  final String label;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final WebfabrikThemeData theme = WebfabrikTheme.of(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SmoothClipRRect(
+          borderRadius: BorderRadius.circular(theme.radii.xLarge),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: theme.spacing.xxMedium),
+            color: theme.colors.translucentBackgroundContrast,
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(theme.spacing.xxSmall),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: theme.colors.error.withValues(alpha: .8),
+                  ),
+                  child: Icon(icon, size: 48, color: theme.colors.background),
+                ),
+                XXMediumGap(),
+                Text(
+                  label,
+                  style: WebfabrikTheme.of(context).text.title3.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: theme.colors.background,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
